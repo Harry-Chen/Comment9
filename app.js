5f9ed85604bf7f0a58fa4f8390617a1c56cfc8b3
王邈 <shankerwangmiao@gmail.com>
2014-11-03 14:09:36 +0800

    add session support

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session    = require('express-session');
+
+var MongoStore = require('connect-mongo')(session);
+var settings = require('./settings');
+
 
 var routes = require('./routes/index');
 //var users = require('./routes/users');
@@ -21,6 +26,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+	secret: settings.cookieSecrect,
+	store: new MongoStore({
+		db: settings.db
+	})
+}));
 
 //app.use('/', routes);
 app.use('/app', routes);
