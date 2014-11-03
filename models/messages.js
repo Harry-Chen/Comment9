4d62da99d7fe1f8fb3f1b5e8c280d8bdb2f73dcf
王邈 <shankerwangmiao@gmail.com>
2014-11-03 17:35:38 +0800

    move approved and messages to mongo db

--- models/messages.js
+++ models/messages.js
@@ -1,16 +1,18 @@
 var settings = require('../settings');
 var mongodb = require('../utils/db');
-var getSeq = require('./uniqueCount');
+var getSeq = require('./uniqueCounter');
 
 function Message(message){
 	this.id = message.id;
 	this.m = message.m;
+	this.approved = !!message.approved;
 }
 module.exports = Message;
 
 Message.prototype.save = function save(callback){
 	var msg = {
 		m: this.m,
+		approved: this.approved
 	};
 	
 	mongodb.open(function(err, db){
@@ -27,16 +29,45 @@ Message.prototype.save = function save(callback){
 					mongodb.close();
 					return callback(err);
 				}
-				collection.ensureIndex('id', {unique: ture});
-				collection.insert(msg, {safe: true}, function(err, user) {
-			￼￼￼￼	mongodb.close();
-					callback(err, user);
+				collection.ensureIndex('id', {unique: true}, function(err){
+					if(err){
+						return callback(err);
+					}
+					collection.insert(msg, {safe: true}, function(err, user){
+						mongodb.close();
+						callback(err, user[0]);
+					});
 				});
 			});
 		});
 	});
 };
-Message.get = function get(id, callback) { 
+Message.prototype.approve = function approve(callback){
+	if(callback === undefined){
+		callback = function(){};
+	}
+	this.approved = true;
+	_this = this;
+	mongodb.open(function(err, db){
+		if(err){
+			return callback(err);
+		}
+		db.collection('messages', function(err, collection){
+			if(err){
+				mongodb.close();
+				return callback(err);
+			}
+			collection.update({id:_this.id}, {$set:{approved:_this.approved}}, {}, function(err, collection){
+				mongodb.close();
+				callback(err, collection);
+			});
+		});
+	});
+}
+Message.get = function get(query, callback,limit) { 
+	if(limit === undefined){
+		limit = 0;
+	}
 	mongodb.open(function(err, db) {
 		if (err) {
 			return callback(err);
@@ -47,12 +78,11 @@ Message.get = function get(id, callback) {
 				mongodb.close(); 
 				return callback(err);
 			}
-			collection.findOne({id: id}, function(err, doc) {
+			collection.find(query, {limit:limit}).sort({id: 1}).toArray(function(err, doc) {
 				mongodb.close(); 
 				if (doc) {
-					// 封装文档为 User 对象
-					var user = new Message(doc);
-					callback(err, user);
+					var msgs = doc.map(function(m){return new Message(m)});
+					callback(err, msgs);
 				} else {
 					callback(err, null);
 				}
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 ClientQueue = require('../utils/clientQueue.js');
 Settings = require('../settings.js');
+var Message = require('../models/messages.js');
 
 var toProcess = [];
-var messages = [];
 var waitingClients = ClientQueue(Settings.longQueryTimeout);
 var approved = [];
 
@@ -14,47 +14,90 @@ router.get('/', function(req, res) {
 });
 router.post('/new', function(req, res){ 
 	//接到请求，存入messages
-	var id = messages.push(req.body.m) - 1;
-	//取出等待的管理员，分配给他
-	var c = waitingClients.getOne();
-	if(c !== undefined){
-		c.json({"id": id, "m": req.body.m});
-		c.end();
-	}else{ 
-	//如果没人在等待，放入队列
-		toProcess.push(id);
-	}
+	//var id = messages.push(req.body.m) - 1;
+	var m = new Message({m: req.body.m});
+	m.save(function(err, newM){
+		if(err){
+			console.log("Error:");
+			console.log(err);
+			return;
+		}else{
+			//取出等待的管理员，分配给他
+			var c = waitingClients.getOne();
+			if(c !== undefined){
+				c.json({"id": newM.id, "m": newM.m});
+				c.end();
+			}else{ 
+			//如果没人在等待，放入队列
+				toProcess.push(newM.id);
+			}
+		}
+	});
 	res.end();
 });
 
-router.get('/admin/fetch', function(req, res){ 
+router.get('/admin/fetch', function(req, res){
 	//检查待处理队列，如果为空
 	if(toProcess.length == 0){
 		//将当前客户端放入等待队列
 		waitingClients.enQueue(res);
 	}else{
-		var m = toProcess.shift();
+		var mid = toProcess.shift();
+		/*
 		res.json({"id": m, "m": messages[m]});
-		res.end();
+		*/
+		Message.get({id: mid}, function(err, ret){
+			var result;
+			if(err || ret.length == 0){
+				console.log("Error:");
+				console.log(err);
+				result = {};
+			}else{
+				result = {
+					id: ret[0].id,
+					m: ret[0].m
+				};
+			}
+			res.json(result);
+			res.end();
+		});
 	}
 });
 
 router.get('/admin/approve/:id', function(req, res){
-	if(messages[parseInt(req.params.id)] !== undefined){
-		approved.push(parseInt(req.params.id));
-	}
+	//if(messages[parseInt(req.params.id)] !== undefined){
+	Message.get({id: parseInt(req.params.id)}, function(err, ret){
+		//console.log(ret);
+		ret.map(function(m){
+			m.approve(console.log);
+		});
+	});
 	res.end();
 });
 router.get('/screen', function(req, res){
 	var start = parseInt(req.query.s);
 	var length = parseInt(req.query.l);
-	res.json(approved.slice(start, start + length).map(function(id, i){
+	/*res.json(approved.slice(start, start + length).map(function(id, i){
 		return {
 			"id": i + start,
 			"m": messages[id],
 		};
-	}));
-	res.end();
+	}));*/
+	Message.get({
+		id: {$gte: start},
+		approved: true
+	}, function(err, ret){
+		if(err){
+			res.end("{}");
+		}
+		res.json(ret.map(function(m){
+			return {
+				id: m.id,
+				m : m.m
+			};
+		}));
+		res.end();
+	}, length);
 });
 
 module.exports = router;
