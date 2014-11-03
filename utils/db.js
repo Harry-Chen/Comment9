1c6bdb704c3b02bd72d141f197b8260511ef71db
王邈 <shankerwangmiao@gmail.com>
2014-11-03 14:14:09 +0800

    bug fix

--- utils/db.js
+++ utils/db.js
@@ -3,4 +3,4 @@ var Db = require('mongodb').Db;
 var Connection = require('mongodb').Connection;
 var Server = require('mongodb').Server;
 
-module.exports = new Db(settings.db, new Server(settings.host, Connection.DEFAULT_PORT, {}));
+module.exports = new Db(settings.db, new Server(settings.host, Connection.DEFAULT_PORT, {}), {safe: true});
