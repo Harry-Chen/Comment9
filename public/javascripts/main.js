68804e591c180c536bac10502f973c9403081602
王邈 <shankerwangmiao@gmail.com>
2014-12-17 17:44:08 +0800

    修改监控的事件

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -32,7 +32,7 @@ $(function getOne(){
 	}
 });
 $(function(){
-	$("body").keyup(function(e){
+	$("body").keypress(function(e){
 		if(e.keyCode == 32 || e.keyCode == 13){
 			e.preventDefault();
 			$("#main tr").eq(0).children().eq(1).trigger('click', e.shiftKey);
