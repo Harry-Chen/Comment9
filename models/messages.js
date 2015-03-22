a038be9f982c0f82ede06be0c236689fc65f032c
zhang <zz593141477@gmail.com>
2015-03-22 11:36:39 +0800

    add index for messages

--- models/messages.js
+++ models/messages.js
@@ -3,13 +3,15 @@ var mongoose = require('../utils/db');
 var getSeq = require('./uniqueCounter');
 
 var messageSchema = mongoose.Schema({
-    id: Number,
+    id: {type: Number, index: true},
     m: String,
     approved: {type: Number, default: 0},
     s: {type: Boolean, default: false},
     received: {type: Date, default: Date.now},
     activity: mongoose.Schema.Types.ObjectId,
 });
+
+messageSchema.index({ activity: 1, approved: 1 });
 
 messageSchema.statics.approveById = function(id, stared, aid, callback){
 	if(!callback){
